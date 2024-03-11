6f65ba090a428b8342292272d5c7f8495fd8a4a9
giosciafra <francescogioscia8@gmail.com>
2024-03-11 17:01:28 +0100

    nav-link popolato dinamicamente e modifiche grafiche

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,26 @@ import AppHeader from './components/AppHeader.vue'
 export default {
    components: {
     AppHeader,
+   },
+
+   data() {
+    return {
+        navLinks: [
+        { text: 'Home', url: '#' },
+        { text: 'About Us', url: '#' },
+        { text: 'Trainings', url: '#' },
+        { text: 'Packages', url: '#' },
+        { text: 'Blog', url: '#' },
+        { text: 'Contacts', url: '#' },
+        ]
+    }
    }
 }
 </script>
 
 <template>
     <div class="container">
-        <AppHeader></AppHeader>
+        <AppHeader :links="navLinks"></AppHeader>
     </div>
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,41 +3,54 @@
 
 export default {
     name: 'AppHeader',
+
+
+    props: {
+        links: {
+            type: Array,
+            required: true,
+        }
+    }
 }
+
+
+
 </script>
 
 <template>
     <div class="container">
-        <nav class="navbar">
-            <div id="logo">
-
-                <img src="/public/img/logo-gobike.png" alt="Logo" class="logo">
-            </div>
-
-            <div id="links">
-
-                <ul class="nav-links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Trainings</a></li>
-                    <li><a href="#">Packages</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Contacts</a></li>
-    
-                </ul>
-            </div>
-
-            <div id="right-nav">
-
-                icon hamb
-
-                <button>
-                    Upcoming Events
-                </button>
+        <nav class="navbar d-flex justify-content-around align-items-center  ">
 
 
-            </div>
+                <div id="logo" class="d-flexalign-items-center h-100">
 
+                    <img src="/public/img/logo-gobike.png" alt="Logo" class="logo img-fluid ">
+                </div>
+
+         
+
+                <div id="links" class="d-flex align-items-center h-100 ">
+
+                    <ul class="nav-links d-flex list-unstyled gap-2   ">
+                        <li v-for="(link, index) in links" :key="index">
+                            <a :href="link.url">{{ link.text }}</a>
+                        </li>
+        
+                    </ul>
+                </div>
+
+                <div id="right-nav" class="d-flex gap-2 h-100 align-items-center pb-3">
+
+                    <div>
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+
+                    <button>
+                        Upcoming Events
+                    </button>
+
+
+                </div>
 
         </nav>
     </div>
@@ -45,6 +58,20 @@ export default {
 
 <style lang="scss">
 @use '../styles/general.scss';
+
+.navbar {
+    height: 92px;
+    img {
+        height: 60px;
+        width: 200px;
+    }
+
+    a {
+        text-decoration: none;
+        list-style: none;
+        color: black;
+    }
+}
 
 
 </style>
